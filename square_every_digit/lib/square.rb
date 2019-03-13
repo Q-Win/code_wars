dee9620f235591da2aa063d28d4e79b487b09e83
Quinn Krug <quinn.krug@gmail.com>
2019-03-13 16:04:58 -0600

    squared every digit complete

--- square_every_digit/lib/square.rb
+++ square_every_digit/lib/square.rb
@@ -1,3 +1,5 @@
 def square_digits(num)
-  9414
+  num.to_s.chars.map do |digit|
+    (digit.to_i)**2
+  end.join.to_i
 end
--- square_every_digit/test/square.rb
+++ square_every_digit/test/square.rb
@@ -8,7 +8,6 @@ class SquareTest < Minitest::Test
 
 
   def test_it_can_find_narcissist_number
-
     assert_equal square_digits(3212),9414
   end
 end

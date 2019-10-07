d399f8a30f2aad6e2b1fb9aba8b43ff2f22d715f
Quinn Krug <quinn.krug@gmail.com>
2019-10-07 13:18:55 -0600

    digital root complete

--- digital_root/lib/digital_root.rb
+++ digital_root/lib/digital_root.rb
@@ -1,3 +1,7 @@
 def digital_root(n)
-  # ...
+  while n > 10 do
+   n = n.to_s.chars.map(&:to_i).sum
+  end
+
+  return n
 end
--- digital_root/test/digital_root_test.rb
+++ digital_root/test/digital_root_test.rb
@@ -9,6 +9,7 @@ class DigitalRootTest < Minitest::Test
 
   def test_it_can_digital_root
     assert_equal digital_root(16),7
-    
+    assert_equal digital_root(942),6
+    assert_equal digital_root(132189),6
   end
 end

b8ebc1016581f2e2a4f2d317307eb65cd5154680
Quinn Krug <quinn.krug@gmail.com>
2019-10-09 17:14:03 -0600

    queue time complete and passing tests

--- queue_time/lib/queue_time.rb
+++ queue_time/lib/queue_time.rb
@@ -7,5 +7,37 @@
 # The function should return an integer, the total time required.
 
 def queue_time(customers, n)
-  #your code here
+  time = 0
+  those_currently_being_served = customers[0..n-1]
+  those_to_be_served = customers[n..-1]
+  while those_currently_being_served.length > 0
+    
+    #sort the currently being served customers by time. Biggest to shortest
+    those_currently_being_served = those_currently_being_served.sort.reverse
+    #take the customer who will take the least amount of time to serve. Use that time to subtract from everyone else.
+
+    amount_to_add_to_time = those_currently_being_served.pop
+
+    #subtract out the amount of time it took to serve the shortest customer from all those being served.
+
+    those_currently_being_served = those_currently_being_served.collect { |i| i - amount_to_add_to_time }
+
+    #remove any that are done being served
+    those_currently_being_served.delete_if {|v| v < 1}
+
+    #add in new customers who've been waiting to be served
+
+    #remove customers from queue
+    for i in 0..(n-those_currently_being_served.length-1) do
+      if those_to_be_served != nil
+        those_currently_being_served << those_to_be_served.shift
+      end
+    end
+
+    those_currently_being_served.delete_if {|v| v == nil}
+
+    time += amount_to_add_to_time
+  end
+
+  time
 end
--- queue_time/test/queue_time_test.rb
+++ queue_time/test/queue_time_test.rb
@@ -8,10 +8,13 @@ class PersistenceTest < Minitest::Test
 
 
   def test_it_can_queue_time
+    assert_equal queue_time([2,2,3,3,4,4], 2), 9
+    assert_equal queue_time([1,2,3,4,5], 1), 15
+    assert_equal queue_time([1,2,3,4,5], 100), 5
     assert_equal queue_time([], 1),0
     assert_equal queue_time([5], 1), 5
     assert_equal queue_time([2], 5), 2
-    assert_equal queue_time([1,2,3,4,5], 1), 15
-    assert_equal queue_time([1,2,3,4,5], 100), 5
+
+
   end
 end

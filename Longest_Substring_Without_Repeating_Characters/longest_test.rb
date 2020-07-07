c216f97cb0cfc390a9e52e23c710411043114f2b
Quinn Krug <quinn.krug@gmail.com>
2020-07-07 09:43:12 -0600

    Set up longest string files

---
+++ Longest_Substring_Without_Repeating_Characters/longest_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './longest'
+require 'pry'
+
+
+class CreatePhoneNumberTest < Minitest::Test
+
+
+  def test_it_can_get_longest_string
+    
+  end
+
+end

a864770289561709531aeaebbfdfb1ea0c890cd8
Quinn Krug <quinn.krug@gmail.com>
2020-07-07 14:10:46 -0600

    progress

--- Longest_Substring_Without_Repeating_Characters/longest.rb
+++ Longest_Substring_Without_Repeating_Characters/longest.rb
@@ -1,3 +1,12 @@
 def length_of_longest_substring(s)
+  used_char = []
+  unique_char = []
+  characters = s.split("")
+  characters.each do |c|
+    if used_char.include?(c)
+      
+    else
 
+    end
+  end
 end

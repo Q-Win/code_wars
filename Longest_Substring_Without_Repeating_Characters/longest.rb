26243fc317a374eca613f4b1a53d3a5163414754
Quinn Krug <quinn.krug@gmail.com>
2020-07-09 13:37:39 -0600

    first soultion to longest substring without repeating characters

--- Longest_Substring_Without_Repeating_Characters/longest.rb
+++ Longest_Substring_Without_Repeating_Characters/longest.rb
@@ -1,10 +1,24 @@
 def length_of_longest_substring(s)
-  all_unique("abccd")
+
+  longest = 0
+  char_array = s.split("")
+  counter_1 = 0
+  for i in char_array do
+    counter_2 = char_array[counter_1..-2].length
+
+    while (counter_2 > 0)
+      if (all_unique(char_array[counter_1..counter_2]) && char_array[counter_1..counter_2].length > longest )
+        longest = char_array[counter_1..counter_2].length
+      end
+      counter_2 -=1
+    end
+    counter_1 +=1
+  end
+  longest
 end
 
-def all_unique(string)
+def all_unique(char_array)
   characters = {}
-  char_array = string.split("")
   for i in char_array do
     if characters[i] == nil
       characters[i] = true
--- Longest_Substring_Without_Repeating_Characters/longest_test.rb
+++ Longest_Substring_Without_Repeating_Characters/longest_test.rb
@@ -7,12 +7,12 @@ require 'pry'
 class CreatePhoneNumberTest < Minitest::Test
 
   def test_it_can_get_unique
-    assert_equal all_unique("abca"), false
-    assert_equal all_unique("abcdrgsq"), true
+    assert_equal all_unique(["a","b","a"]), false
+      assert_equal all_unique(["a","q","u","y","t","r","e","w"]), true
   end
 
-  # def test_it_can_get_longest_string
-  #   assert_equal length_of_longest_substring("abcabcbb"), 3
-  # end
+  def test_it_can_get_longest_string
+    assert_equal 3, length_of_longest_substring("abcabcbb")
+  end
 
 end
